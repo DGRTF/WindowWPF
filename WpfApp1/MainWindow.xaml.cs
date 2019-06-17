b1ea8c611c72a520ab18092504e2529d4b599184
DGRTF <Roxy123@mail.ru>
2019-06-18 02:09:41 +0300

    c2

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public static class WinCommand
+    {
+        public static RoutedUICommand MaximizeWindow { get; } = new RoutedUICommand("MaximizeWindow", "MaximizeWindow", typeof(MainWindow));
+        public static RoutedUICommand MinimizeWindow { get; } = new RoutedUICommand("MinimizeWindow", "MinimizeWindow", typeof(MainWindow));
+
+    }
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        private void MaxWin(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+                this.WindowState = WindowState.Maximized;
+            else
+                this.WindowState = WindowState.Normal;
+        }
+
+        private void MinWin(object sender, ExecutedRoutedEventArgs e)
+        {
+                this.WindowState = WindowState.Minimized;
+        }
+        private void CloseCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            //MessageBox.Show(WinCommand.MaximizeWindow.OwnerType.ToString());
+            //Console.Read();
+            this.Close();
+        }
+
+        bool isWiden;
+
+        private void Rectangle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            isWiden = true;
+        }
+
+        private void Rectangle_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            isWiden = false;
+            Rectangle rect = (Rectangle)sender;
+            rect.ReleaseMouseCapture();
+        }
+
+        private void Rectangle_MouseMove(object sender, MouseEventArgs e)
+        {
+            Rectangle rect = (Rectangle)sender;
+            if (isWiden)
+            {
+                rect.CaptureMouse();
+                double newWidth = e.GetPosition(this).X + 5;
+
+                if (newWidth > 0)
+                    this.Width = newWidth;
+            }
+        }
+        private void Rectangle_MouseMove1(object sender, MouseEventArgs e)
+        {
+            Rectangle rect = (Rectangle)sender;
+            if (isWiden)
+            {
+                rect.CaptureMouse();
+                double newWidth = e.GetPosition(this).Y + 5;
+
+                if (newWidth > 0)
+                    this.Height = newWidth;
+            
+            }
+        }
+        private void titleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
